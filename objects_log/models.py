c126d1c36376a069d4ded25e2a78a096a02b17a6
michalzg <michalzejmo@gmail.com>
2020-12-27 21:56:10 +0100

    init migration

--- objects_log/models.py
+++ objects_log/models.py
@@ -4,13 +4,16 @@ from django.db import models
 class Target(models.Model):
     datatime_start = models.DateTimeField()
     datetime_end = models.DateTimeField()
+    observer = models.CharField(max_length=254)
     name = models.CharField(max_length=257, unique=True)
     ra = models.DecimalField(max_digits=8, decimal_places=5,
         null=True, blank=True)
     dec = models.DecimalField(max_digits=8, decimal_places=5,
         null=True, blank=True)
     note = models.TextField(max_length=511, null=True, blank=True)
-    program = models.ForeignKey('obs_log.Program', null=True, 
+    telescope = models.ForeignKey('objects_log.Telescope',
+        on_delete=models.DO_NOTHING)
+    program = models.ForeignKey('objects_log.Program', null=True, 
         blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
@@ -37,6 +40,14 @@ class Program(models.Model):
     name = models.CharField(max_length=257, unique=True)
     description = models.TextField(max_length=1023, null=True, blank=True)
     author = models.CharField(max_length=257, unique=True)
+    note = models.TextField(max_length=1023, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Telescope(models.Model):
+    name = models.CharField(max_length=257, unique=True)
+    description = models.TextField(max_length=1023, null=True, blank=True)
     note = models.TextField(max_length=1023, null=True, blank=True)
 
     def __str__(self):
--- observatory_logs/settings.py
+++ observatory_logs/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'objects_log.apps.ObjectsLogConfig',
+    # 'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
