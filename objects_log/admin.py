4d66e2934b7f61386efd89932290cd459cb161ac
michalzg <michalzejmo@gmail.com>
2021-01-04 01:09:53 +0100

    migrations

--- objects_log/admin.py
+++ objects_log/admin.py
@@ -1,32 +1,39 @@
 from django.contrib import admin
-from .models import Target, Observer, ColorFilter, Program, Telescope
+from .models import Target, Observer, ColorFilter, Program, Telescope, Tag
 from django.conf.locale.en import formats as en_formats
 from django.conf.locale.pl import formats as pl_formats
 
 en_formats.DATETIME_FORMAT = "d-m-y H:i:s"
 pl_formats.DATETIME_FORMAT = "d-m-y H:i:s"
+NOTE_DISPLAY_LENGHT = 10
+
 
 class TargetAdmin(admin.ModelAdmin):
     
     def colorfilter_display(self, obj):
         return ', '.join([f.name for f in obj.colorfilter.all()])
+    
+    def tag_display(self, obj):
+        return ', '.join([f.name for f in obj.tag.all()])
 
-    def target_program(self, obj):
-        return obj.program
+    def note_display(self, obj):
+        if len(obj.note) > NOTE_DISPLAY_LENGHT:
+            return f'{obj.note[:NOTE_DISPLAY_LENGHT]}...'
+        return obj.note
 
-    list_display = ('id', 'name', 'datetime_start',
-        'colorfilter_display', 'program', 'telescope', 'number_of_frames', 'note')
-    list_display_links = ['id']
-    list_editable = ('name', 'program')
+    list_display = ('name', 'datetime_start', 'observer',
+        'colorfilter_display', 'program', 'telescope', 'number_of_frames', 'note_display', 'tag_display')
+    list_editable = ('program',)
 
     colorfilter_display.short_description = 'Filters'
-    target_program.short_description = 'Program'
+    tag_display.short_description = 'Tags'
     # program.empty_value_display = '???'
 
     class Media:
         css = {"all": ("objects_log/css/style.css",)}
 
 admin.site.register(Target, TargetAdmin)
+admin.site.register(Tag)
 admin.site.register(Telescope)
 admin.site.register(Observer)
 admin.site.register(ColorFilter)
--- objects_log/models.py
+++ objects_log/models.py
@@ -5,7 +5,7 @@ class Target(models.Model):
     datetime_start = models.DateTimeField()
     datetime_end = models.DateTimeField()
     observer = models.CharField(max_length=254, null=True, blank=True)
-    name = models.CharField(max_length=257, unique=True)
+    name = models.CharField(max_length=257)
     ra = models.DecimalField(max_digits=12, decimal_places=10,
         null=True, blank=True, help_text='RA in decimal hours')
     dec = models.DecimalField(max_digits=12, decimal_places=10,
@@ -15,10 +15,11 @@ class Target(models.Model):
     program = models.ForeignKey('objects_log.Program', null=True, 
         blank=True, on_delete=models.SET_NULL)
     colorfilter = models.ManyToManyField('objects_log.ColorFilter')
-    number_of_frames = models.IntegerField()
+    tag = models.ManyToManyField('objects_log.Tag', blank=True)
+    number_of_frames = models.IntegerField(blank=True, null=True)
+
     def __str__(self):
         return f'{self.name}'
-
 
 class Observer(models.Model):
     name = models.CharField(max_length=257, unique=True)
@@ -46,7 +47,7 @@ class ColorFilter(models.Model):
 class Program(models.Model):
     name = models.CharField(max_length=257, unique=True)
     description = models.TextField(max_length=1023, null=True, blank=True)
-    author = models.CharField(max_length=257, unique=True)
+    author = models.CharField(max_length=257)
     note = models.TextField(max_length=1023, null=True, blank=True)
 
     def __str__(self):
@@ -57,6 +58,20 @@ class Telescope(models.Model):
     name = models.CharField(max_length=257, unique=True)
     description = models.TextField(max_length=1023, null=True, blank=True)
     note = models.TextField(max_length=1023, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=12, unique=True)
+    weight = models.IntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(0)
+        ], help_text='For display sorting purposes'
+    )
 
     def __str__(self):
         return f'{self.name}'
