077c9619dc1f97c1e7860e60ca1a168ee7238bed
michal_z <michalzejmo@gmail.com>
2021-02-17 16:59:36 +0100

    create colorfilter on save

--- objects_log/serializers.py
+++ objects_log/serializers.py
@@ -11,22 +11,29 @@ class ColorFilterSerializer(serializers.ModelSerializer):
     class Meta:
         model = ColorFilter
         fields = ('name', )
+        extra_kwargs = {
+            'name': {'validators': []},
+        }
 
 class TelescopeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Telescope
         fields = ('name', )
-        read_only_fields = ('name',)
-
+        # read_only_fields = ('name',)
+        extra_kwargs = {
+            'name': {'validators': []},
+        }
 
 class TargetSerializer(serializers.ModelSerializer):
     telescope = serializers.SlugRelatedField(
         many=False, read_only=False, queryset=Telescope.objects.all(),
         slug_field='name')
-    colorfilters = serializers.SlugRelatedField(
-        many=True, read_only=False, queryset=ColorFilter.objects.all(),
-        slug_field='name'
-    )
+    # colorfilters = serializers.SlugRelatedField(
+    #     many=True, read_only=False, queryset=ColorFilter.objects.all(),
+    #     slug_field='name'
+    # )
+    # telescope = TelescopeSerializer(many=False)
+    colorfilters = ColorFilterSerializer(many=True)
 
     class Meta:
         model = Target
@@ -39,6 +46,20 @@ class TargetSerializer(serializers.ModelSerializer):
                 fields=['datetime_start', 'telescope',]
             )
         ]
+
+    def create(self, validated_data):
+        colorfilters_data = validated_data.pop('colorfilters')
+        # telescope_data = validated_data.pop('telescope') 
+
+        target = Target.objects.create(**validated_data)
+        if colorfilters_data:
+            for c_filter_data in colorfilters_data:
+                c_filter, _ = ColorFilter.objects.get_or_create(
+                    **c_filter_data
+                )
+                target.colorfilters.add(c_filter)
+
+        return target
 
 class TargetStatsSerializer(serializers.BaseSerializer):
     def to_representation(self, instance):
