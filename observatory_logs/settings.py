103af770f397120f108bae2eb3ce603dbbab333d
michal_z <michalzejmo@gmail.com>
2021-02-20 16:44:08 +0100

    cor3

--- observatory_logs/settings.py
+++ observatory_logs/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'grappelli',
     'objects_log.apps.ObjectsLogConfig',
     'rest_framework',
-    # 'corsheaders',
-    # 'bootstrap_admin',
+    'corsheaders',
+    'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -59,8 +59,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
-    # 'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'observatory_logs.urls'
