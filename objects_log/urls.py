130edb5d37271a533f6b738cab5f1c3c78569314
michal_z <michalzejmo@gmail.com>
2021-03-23 14:39:42 +0100

    admin urls

--- objects_log/urls.py
+++ objects_log/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from objects_log import views
+from django.contrib import admin
 
 urlpatterns = [
+    path('', admin.site.urls),
     path('targets/', views.target_list),
     path('targets/<int:pk>/', views.target_detail),
     path('stats/targets/', views.targets_stats),
